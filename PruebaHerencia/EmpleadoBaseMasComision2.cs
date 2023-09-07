163bd2807e690ef9ff6e37f59d1d407a30bb167c
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-09-07 10:04:34 -0600

    Prueba Herencia

---
+++ PruebaHerencia/EmpleadoBaseMasComision2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaHerencia
+{
+    public class EmpleadoBaseMasComision2 : EmpleadoPorComision
+    {
+        private decimal salarioBase;
+        public EmpleadoBaseMasComision2(string nombre, string apellido, 
+            string nss, decimal tarifa, decimal venta, decimal salario) 
+            : base(nombre, apellido, nss, tarifa, venta)
+        {
+            salarioBase = salario;
+        }
+        public decimal SalarioBase 
+        { 
+            get 
+            { 
+                return salarioBase; 
+            } 
+            set 
+            { 
+                salarioBase = (value < 0) ? 0 : value;
+            } 
+        }
+
+        public override decimal Ingresos()
+        {
+            return salarioBase + base.Ingresos();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}\n{2}: {3:C}",
+                "salario base +", base.ToString(),
+                "salario base", SalarioBase);
+        }
+    }
+}
---
+++ PruebaHerencia/Program.cs
@@ -0,0 +1,24 @@
+﻿using PruebaHerencia;
+
+EmpleadoPorComision empleadoPorComision = new("Susana", "Figueroa",
+    "222-22-2222", .06M, 10000.00M);
+
+Console.WriteLine("{0} {1}:\n\n{2}\n{3}: {4:C}\n",
+    "Llama a ToString de EmpleadoPorComision con referencia de clase base",
+    "a objeto de clase base", empleadoPorComision.ToString(),
+    "ingresos", empleadoPorComision.Ingresos());
+
+EmpleadoBaseMasComision2 empleadoBaseMasComision = new("Roberto",
+    "López", "333-333-3333", .04M, 5000.00M, 300.00M);
+
+Console.WriteLine("{0} {1}:\n\n{2}\n{3}: {4:C}\n",
+    "Llama a ToString de EmpleadoBaseMasComision con referencia de clase",
+    "derivada a objeto de clase derivada", empleadoBaseMasComision.ToString(),
+    "ingresos", empleadoBaseMasComision.Ingresos());
+
+EmpleadoPorComision empleadoPorComision2 = empleadoBaseMasComision;
+
+Console.WriteLine("{0} {1}:\n\n{2}\n{3}: {4:C}\n",
+    "Llama a ToString de EmpleadoBaseMasComision con referencia de clase base",
+    "a objeto de clase derivada", empleadoPorComision2.ToString(),
+    "ingresos", empleadoPorComision2.Ingresos());
