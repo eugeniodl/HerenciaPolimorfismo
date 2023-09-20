a4b75dc692015e9cb7ff297ffdc67dc5f1a795fe
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-09-20 10:25:02 -0600

    PayableInterfaceTest

---
+++ PayableInterfaceTest/IPayable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayableInterfaceTest
+{
+    // IPayable interface declaration
+    public interface IPayable
+    {
+        decimal GetPaymentAmount(); // calculate payment; no implementation
+    }
+}
---
+++ PayableInterfaceTest/Invoice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayableInterfaceTest
+{
+    // Invoice class implements IPayable
+    public class Invoice : IPayable
+    {
+        public decimal GetPaymentAmount()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
